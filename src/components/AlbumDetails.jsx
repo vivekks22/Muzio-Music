a44c01a299f040ccef248cfec99ac2fb82026266
vivekks22 <ksvivek7777@gmail.com>
2024-11-04 16:24:39 +0530

    Added MediaSession

--- src/components/AlbumDetails.jsx
+++ src/components/AlbumDetails.jsx
@@ -306,49 +306,6 @@ if (indexToRemove !== -1) {
 
 
 
-  // const initializeMediaSession = () => {
-  //   if ("mediaSession" in navigator) {
-  //     navigator.mediaSession.metadata = new MediaMetadata({
-  //       title: songlink[0]?.name || "",
-  //       artist: songlink[0]?.album?.name || "",
-  //       artwork: [
-  //         {
-  //           src: songlink[0]?.image[2]?.url || "",
-  //           sizes: "512x512",
-  //           type: "image/jpeg",
-  //         },
-  //       ],
-  //     });
-  
-  //     navigator.mediaSession.setActionHandler("play", function () {
-  //       // Handle play action
-  //       if (audioRef.current) {
-  //         audioRef.current.play().catch((error) => {
-  //           console.error("Play error:", error);
-  //         });
-  //       }
-  //     });
-  
-  //     navigator.mediaSession.setActionHandler("pause", function () {
-  //       // Handle pause action
-  //       if (audioRef.current) {
-  //         audioRef.current.pause().catch((error) => {
-  //           console.error("Pause error:", error);
-  //         });
-  //       }
-  //     });
-  
-  //     navigator.mediaSession.setActionHandler("previoustrack", function () {
-  //       pre();
-  //     });
-  
-  //     navigator.mediaSession.setActionHandler("nexttrack", function () {
-  //       next();
-  //     });
-  //   } else {
-  //     console.warn("MediaSession API is not supported.");
-  //   }
-  // };
 
   const initializeMediaSession = () => {
     const isIOS = /(iPhone|iPod|iPad)/i.test(navigator.userAgent);
@@ -510,7 +467,49 @@ if (indexToRemove !== -1) {
   
     if (!isIOS && songlink.length > 0) {
       audioRef.current.play();
-      initializeMediaSession();
+      (() => {
+        if ("mediaSession" in navigator) {
+          navigator.mediaSession.metadata = new MediaMetadata({
+            title: songlink[0]?.name || "",
+            artist: songlink[0]?.album?.name || "",
+            artwork: [
+              {
+                src: songlink[0]?.image[2]?.url || "",
+                sizes: "512x512",
+                type: "image/jpeg",
+              },
+            ],
+          });
+
+          navigator.mediaSession.setActionHandler("play", function () {
+            // Handle play action
+            if (audioRef.current) {
+              audioRef.current.play().catch((error) => {
+                console.error("Play error:", error);
+              });
+            }
+          });
+
+          navigator.mediaSession.setActionHandler("pause", function () {
+            // Handle pause action
+            if (audioRef.current) {
+              audioRef.current.pause().catch((error) => {
+                console.error("Pause error:", error);
+              });
+            }
+          });
+
+          navigator.mediaSession.setActionHandler("previoustrack", function () {
+            pre();
+          });
+
+          navigator.mediaSession.setActionHandler("nexttrack", function () {
+            next();
+          });
+        } else {
+          console.warn("MediaSession API is not supported.");
+        }
+      })();
     }
   }, [songlink]);
 
