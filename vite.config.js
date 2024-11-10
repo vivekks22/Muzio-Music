5c66fdaa760ef187c6cddd4a52b78e9c9fcf4d30
vivekks22 <ksvivek7777@gmail.com>
2024-11-10 09:56:13 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['react-icons/fa'], // add this line to externalize the icons module
     },
   },
-});
+);
